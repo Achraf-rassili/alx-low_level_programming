f2bb3c3c38f431f9d2eaa0c1130f4f42de0964b3
Achraf rassili <ashraf@rassili.com>
2023-03-22 01:45:12 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase
- */
-void print_alphabet(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
-}
-
-/**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
@@ -23,6 +10,7 @@ void print_alphabet_x10(void)
 	int i;
 
 	i = 0;
+
 	while (i < 10)
 	{
 		ch = 'a';
