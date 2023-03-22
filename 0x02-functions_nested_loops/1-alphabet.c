557bf19ca7f168a950327e314ce50c546e58291f
Achraf rassili <ashraf@rassili.com>
2023-03-22 01:29:08 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
+/**
+ * main - Entry point
+ *
+ * Description: Prints alphabet 
+void print_alphabet(void)
 	{
 		char letter;
 
