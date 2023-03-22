c35eb970c99deb209f9eef014ea35720a1007b3d
Achraf rassili <ashraf@rassili.com>
2023-03-22 02:47:07 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,11 @@
 
 	/**
 	 * print_times_table - prints the n times table, starting with 0
-	 * @n: number of the times table
+	 * @n: number of the times table (0 <= n <= 15)
+	 *
+	 * Description: This function prints the n times table, starting with 0, up to n * n.
+	 * Each line represents a different multiplier, and each column represents a different
+	 * number of times that multiplier is applied to.
 	 */
 	void print_times_table(int n)
 	{
