c9a6639d8c37348077a4b99cd427baaf95ba02cd
Achraf rassili <ashraf@rassili.com>
2023-03-22 01:55:03 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
-
-	/**
-	 * _isalpha - checks for alphabetic character
-	 * @c: the character to be checked
-	 * Return: 1 if c is a letter, 0 otherwise
-	 */
-	int _isalpha(int c)
-	{
-		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-	}
+/**
+ * _isalpha - checks for an alphabetic character
+ * @c: the character to be checked
+ * 
+ * Return: 1 if c is an alphabetic character, 0 otherwise
+ * 
+ * Description:
+ * This function checks if the given character is an alphabetic character.
+ * Alphabetic characters are defined as either uppercase or lowercase letters.
+ */
+int _isalpha(int c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
