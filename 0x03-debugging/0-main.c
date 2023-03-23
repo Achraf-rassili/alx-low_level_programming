bcca2c02d247e03dab0ba06b58bc7d2e666a5c05
Achraf rassili <ashraf@rassili.com>
2023-03-23 14:40:18 +0000

    0-main.c Corr

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point to test 
- * whether a number is positive or negative
+ * main - Entry point to test whether a number is positive or negative
  *
  * Return: Always 0 (Success)
  */
