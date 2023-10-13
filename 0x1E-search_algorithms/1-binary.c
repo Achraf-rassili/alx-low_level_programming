18f1069fcf1db7a83901fc5409bec983621a64de
root <ashraf@rassili.com>
2023-10-12 17:53:16 -0700

    first task correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,53 +1,50 @@
-#include <stdio.h>
 #include "search_algos.h"
+
 /**
- * binary_search - searches for a value in a sorted array of integers
- * using the Binary search algorithm
- * @array: array of integers to be searched
- * @size: size of the array
- * @value: value tp be searched for
- * Return: index or -1
+ * binary_search - Searches for a value in a sorted array using binary search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, return -1.
+ *         Otherwise, return the index where the value is located.
+ *
+ * Description: Prints the subarray being searched after each change.
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array)
-		return (_search(array, 0, size - 1, value));
+	size_t left, right, mid, i;
+
+	/* Check for a NULL array */
+	if (array == NULL)
+		return (-1);
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		/* Calculate the midpoint of the current search range */
+		mid = left + (right - left) / 2;
+
+		/* Print the subarray being searched */
+		printf("Searching in array: %d", array[left]);
+		for (i = left + 1; i <= right; i++)
+			printf(", %d", array[i]);
+		printf("\n");
+
+		/* Check if the value is found at the midpoint */
+		if (array[mid] == value)
+			return (mid);
+
+		/* If the value is in the left half, update the right index */
+		if (array[mid] > value)
+			right = mid - 1;
+		/* If the value is in the right half, update the left index */
+		else
+			left = mid + 1;
+	}
+
+	/* Value not found in the array */
 	return (-1);
-}
-
-/**
- * _search - searches for value
- * @array: int ordered array
- * @left: the left-most index of the array
- * @right: the right-most index of array
- * @value: int searched for
- * Return: -1 or index
- */
-int _search(int *array, int left, int right, int value)
-{
-	int mid, i;
-
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-		printf("%i, ", array[i]);
-	printf("%i\n", array[i]);
-	mid = (right + left) / 2;
-	if (right - left == 0)
-	{
-		if (array[mid] != value)
-			return (-1);
-	}
-
-	if (array[mid] < value)
-	{
-		left = mid + 1;
-		return (_search(array, left, right, value));
-	}
-	else if (array[mid] > value)
-	{
-		right = mid - 1;
-		return (_search(array, left, right, value));
-	}
-	else
-		return (mid);
 }
